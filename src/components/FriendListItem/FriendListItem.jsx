f9c775aa8662984562d9edfaec911617bdf31d12
Dmytro Lyfenko <d.lyfenko@gmail.com>
2024-07-27 23:56:39 +0300

    fix

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,15 @@
+import styles from './FriendListItem.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div className={styles.friendListItem}>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+      <p className={isOnline ? styles.statusOnline : styles.statusOffline}>
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
+    </div>
+  );
+};
+
+export default FriendListItem;
